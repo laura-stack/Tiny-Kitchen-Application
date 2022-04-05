6178655f38d2a7feb060d16eaf7611bd2aaae327
LauraLopez <llopez11@my.apsu.edu>
2022-04-05 10:22:09 -0500

    Added icons to FindRecipe, Collections, Pantry, and Settings Buttons. Also added functionality of creating a new window when each of the previously mentioned buttons are pressed.

---
+++ Tiny Kitchen Application/FindRecipesWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Tiny_Kitchen_Application
+{
+    /// <summary>
+    /// Interaction logic for FindRecipesWindow.xaml
+    /// </summary>
+    public partial class FindRecipesWindow : Window
+    {
+        public FindRecipesWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- Tiny Kitchen Application/obj/Debug/netcoreapp3.1/App.g.cs
+++ Tiny Kitchen Application/obj/Debug/netcoreapp3.1/App.g.cs
@@ -47,7 +47,7 @@ namespace Tiny_Kitchen_Application {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.3.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.11.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -73,7 +73,7 @@ namespace Tiny_Kitchen_Application {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.3.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.11.0")]
         public static void Main() {
             Tiny_Kitchen_Application.App app = new Tiny_Kitchen_Application.App();
             app.InitializeComponent();
