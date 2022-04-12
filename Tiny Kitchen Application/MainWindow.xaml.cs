9f8bdfb2765d5c4a88aa252c5265f4e497066c7e
Autumn Hale <ahale6@my.apsu.edu>
2022-04-12 18:16:25 -0500

    Added icon, color change, deleted .cs classes.

--- Tiny Kitchen Application/MainWindow.xaml.cs
+++ Tiny Kitchen Application/MainWindow.xaml.cs
@@ -74,6 +74,14 @@ namespace Tiny_Kitchen_Application
 			settingsWindow.Width = 450;
 		}
 
+		public void CalendarButton_Click(object sender, RoutedEventArgs e)
+		{
+			SettingsWindow settingsWindow = new SettingsWindow();
+			settingsWindow.Show();
+			settingsWindow.Height = 450;
+			settingsWindow.Width = 450;
+		}
+
 
 
 	}
