fed077a164b93bff934a4949f1921daadbbb4cf9
LauraLopez <llopez11@my.apsu.edu>
2022-04-22 14:11:02 -0500

    user can now add items to pantry array list one at a time. needs fixing so that way they can continue clicking the 'add' button. Next steps-> fix previously mentioned issue and create a delete from pantry method.

--- Tiny Kitchen Application/PantryWindow.xaml.cs
+++ Tiny Kitchen Application/PantryWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace Tiny_Kitchen_Application
 {
@@ -18,32 +19,72 @@ namespace Tiny_Kitchen_Application
     /// Interaction logic for PantryWindow.xaml
     /// </summary>
     ///
+
     public class Pantry
     {
+
         ArrayList items;
 
-        Pantry()
+        public Pantry()
         {
-            items = new ArrayList(); 
+            items = new ArrayList();
         }
 
+        public ArrayList getItems()
+        {
+            return items;
+        }
         public void AddItem(String item)
         {
-            items.Add(item); 
+            items.Add(item);
         }
 
         public void DeleteItem(String item)
         {
-            items.Remove(item); 
+            items.Remove(item);
         }
 
     }
 
     public partial class PantryWindow : Window
     {
+        Pantry pantry = new Pantry();
+        //public System.Windows.Forms.ListBox.ObjectCollection Items { get; }
+        //ListBox listbox = new ListBox(); 
+
+        string path = @"C:\Users\chach\source\repos\Tiny Kitchen Application\Tiny Kitchen Application\Pantry.txt";
         public PantryWindow()
         {
             InitializeComponent();
+        }
+
+        public void AddToPantry_Click(object sender, RoutedEventArgs e)
+        {
+            Pantry_Popup.IsOpen = true;
+        }
+
+        public void SaveItem_Click(object sender, RoutedEventArgs e)
+        {
+            pantry.AddItem(pantryItem.Text);
+            Pantry_Popup.IsOpen = false;
+
+            AddPantryItem();
+        }
+
+        public void AddPantryItem()
+        {
+
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine(pantryItem.Text);
+            }
+        }
+
+        // public void 
+
+        public void ExitPantryAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Pantry_Popup.IsOpen = false;
         }
 
 
